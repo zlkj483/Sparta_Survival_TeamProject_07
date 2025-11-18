07512a32b8de275b5386f48fada8c0efafb1c846
경칠이람 <kkc1590753@gmail.com>
2025-11-18 11:44:57 +0900

    feat: 건축 불가 판정, 건축 UI, 건축 데이터 작성

---
+++ Assets/Scripts/Build/ScriptableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuildingData", menuName = "Game/Building Data")]
+public class BuildingData : ScriptableObject
+{
+    public string buildingName;
+    public Sprite icon;
+    public string description;
+
+    [System.Serializable]
+    public class LevelInfo
+    {
+        public int requiredWood;
+        public int requiredStone;
+        public float buildTime;
+        public GameObject prefab;   // 해당 단계의 실제 Prefab
+    }
+
+    public LevelInfo[] levels;
+}
