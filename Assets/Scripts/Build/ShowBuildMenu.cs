4fef1b53689f15fb2cf8fdaf2d039f6a8389bc4d
경칠이람 <kkc1590753@gmail.com>
2025-11-20 02:02:19 +0900

    feat: 건축 UI RightPanel 완성

---
+++ Assets/Scripts/Build/ShowBuildMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShowBuildMenu : MonoBehaviour
+{
+    public GameObject buildMenuPanel;
+
+    public void OnBuildMenu(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            bool active = buildMenuPanel.activeSelf;
+            buildMenuPanel.SetActive(!active);
+        }
+    }
+}
