c608eb022113957613dba177b189adc8e8f5e96c
김동관 <kdk7992@gmail.com>
2025-11-19 21:00:46 +0900

    feat: IGatherable struture에 상속 추가

--- Assets/Scripts/Structure/Structure.cs
+++ Assets/Scripts/Structure/Structure.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Structure : MonoBehaviour
+public class Structure : MonoBehaviour, IGatherable
 {
     public StructureData data;
     private int accumulatedDamage = 0;
